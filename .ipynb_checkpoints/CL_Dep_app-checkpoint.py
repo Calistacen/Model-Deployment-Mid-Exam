929606692dcaef61488ba0614dddfff875983f39
Liana <lianardicalista@gmail.com>
2025-04-18 20:04:55 +0700

    Initial commit

---
+++ .ipynb_checkpoints/CL_Dep_app-checkpoint.py
@@ -0,0 +1,108 @@
+import pickle
+import numpy as np
+import streamlit as st
+
+def predict_booking_status(input_data):
+    with open('CL_ranfor_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    prediction = model.predict(np.array(input_data).reshape(1, -1))
+    return prediction
+
+def main():
+    st.title("Hotel Booking Status Prediction")
+
+    st.markdown("### Guest Information")
+    adults = st.number_input("Number of Adults", 1, 4, step=1)
+    children = st.number_input("Number of Children", 0, step=1)
+
+    market_options = {
+        "Online": 4,
+        "Offline": 3,
+        "Corporate": 2,
+        "Complementary": 1,
+        "Aviation": 0
+    }
+    market = st.selectbox("Type of Booking", list(market_options.keys()))
+    market_value = market_options[market]
+
+    st.markdown("### Stay Duration")
+    weekday_nights = st.slider("Weekday Nights (Mon - Fri)", 0, 30)
+    remaining = 30 - weekday_nights
+    weekend_nights = st.slider("Weekend Nights", 0, remaining)
+
+    st.markdown("### Parking & Requests")
+    required_car_parking_space = int(st.checkbox("Guest need parking slot?"))
+    special_requests = st.slider("Special Requests", 0, 5)
+
+    st.markdown("### Booking Info")
+    lead_time = st.number_input("How many days guest booked before check-in?", 0, step=1)
+    year = st.selectbox("Arrival Year", [2017, 2018])
+
+    month_options = {
+        "January": 1, 
+        "February": 2, 
+        "March": 3, 
+        "April": 4,
+        "May": 5, 
+        "June": 6, 
+        "July": 7, 
+        "August": 8,
+        "September": 9, 
+        "October": 10, 
+        "November": 11, 
+        "December": 12 
+    }
+    month = st.selectbox("Arrival Month", list(month_options.keys()))
+    month_value = month_options[month]
+
+    day = st.number_input("Arrival Day", min_value=1, max_value=31, step=1)
+    avg_price = st.number_input("Avg Price per Room (Euro)", 0.0)
+    room_type = st.radio("Type of Room Booked", [1, 2, 3, 4, 5, 6, 7])
+    meal_options = {
+        "Not Selected": 3,
+        "Meal Plan 1": 0,
+        "Meal Plan 2": 1,
+        "Meal Plan 3": 2
+    }
+    meal = st.selectbox("Type of Meal Plan Choosen", list(meal_options.keys()))
+    meal_value = meal_options[meal]
+
+    st.markdown("### Guest History")
+    repeated_guest = int(st.checkbox("Guest ever booked before?"))
+    if repeated_guest:
+        previous_booking = st.number_input("Number of previous booking made", min_value = 1)
+        previous_cancelation = st.number_input("Guest cancelation before this booking", 0)
+    else:
+        previous_booking = 0
+        previous_cancelation = 0
+    
+    
+    input_data = [adults, children, weekend_nights, weekday_nights, meal_value, required_car_parking_space, room_type, lead_time, year, month_value, day, market_value, repeated_guest, previous_cancelation, previous_booking, avg_price, special_requests]
+
+    
+    if st.button("Predict"):
+        with st.spinner("Processing your booking... Please wait."):
+            prediction = predict_booking_status(input_data)
+            
+        if prediction == 0:
+            st.error("❌ The guest will most likely cancel this booking.")
+        elif prediction == 1:
+            st.success("✅ The guest will most likely proceed with this booking.")
+        else:
+            st.warning("⚠️ Unable to determine the booking status.")
+
+st.markdown("""
+    <style>
+    .watermark {
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+        color: lightgray;
+        font-size: 13px;
+    }
+    </style>
+    <div class="watermark">Made by Calista Lianardi - 2702325880</div>
+    """, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
---
+++ .ipynb_checkpoints/CL_RanFor_OOP-checkpoint.py
@@ -0,0 +1,49 @@
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+class CalsNetRandomForest:
+    def __init__(self, criterion='entropy', n_estimators=300, max_depth=30, random_state=42, min_samples_leaf=1, 
+    min_samples_split=2):
+        self.model = model if model else RandomForestClassifier(criterion=criterion, 
+                                                                n_estimators=n_estimators, 
+                                                                max_depth=max_depth, 
+                                                                random_state=random_state,
+                                                                min_samples_leaf=min_samples_leaf, 
+                                                                min_samples_split=min_samples_split)
+    
+    def train(self, x_train, y_train):
+        """Trains the model with the provided training data."""
+        self.model.fit(x_train, y_train)
+    
+    def predict(self, x_test):
+         """Predicts the target values for the given test data later."""
+        return self.model.predict(x_test)
+    
+    def evaluate(self, x_test, y_test):
+        """Evaluates the model using accuracy, classification report, and confusion matrix."""
+        predictions = self.predict(x_test)
+        acc = accuracy_score(y_test, predictions)
+        evaluation = {
+            "Accuracy": acc,
+            "Classification Report": classification_report(y_test, predictions),
+            "Confusion Matrix": confusion_matrix(y_test, predictions)
+        }
+        return evaluation
+    
+    def save(self, path='ranfor_model.pkl'):
+        """Saves the trained model to a specified path."""
+        try:
+            with open(path, 'wb') as f:
+                pickle.dump(self.model, f)
+        except Exception as e:
+            print(f"Error saving model: {e}")
+    
+    def load(self, path='CL_ranfor_model.pkl'):
+        """Loads a model from a specified path."""
+        try:
+            with open(path, 'rb') as f:
+                self.model = pickle.load(f)
+            print(f"Model loaded from {path}")
+        except Exception as e:
+            print(f"Error loading model: {e}")
